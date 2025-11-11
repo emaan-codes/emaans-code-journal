df829f3037957a1d096b8539746b350a2a61a7bc
Emaan <emaanmalikk59@gmail.com>
2025-11-11 10:15:28 +0500

    add css styled navbar and made it responsive

--- script.js
+++ script.js
@@ -47,3 +47,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });   
 });
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  });
+});
